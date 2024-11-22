4ca475e6b0b84825b4216d504312ecca74141b21
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-11-22 22:33:12 +0200

    Migrated back to Flask since threading locks were not working as well with asynchronous FastAPI

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:bookworm AS build-stage
 
 WORKDIR /usr/src/app
 
-RUN pip install llama-cpp-python ctransformers fastapi uvicorn
+RUN pip install llama-cpp-python ctransformers flask flask_cors waitress
 
 RUN wget -O model https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q2_K.bin?download=true
 
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from ctransformers import AutoModelForCausalLM
 import time
-from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
-import urllib.parse
+from flask import Flask, request, Response
+from flask_cors import CORS
+from waitress import serve
 import threading
-import uvicorn
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info(f"Loading model...")
 
 start = time.time()
 
@@ -14,37 +18,27 @@ llm = AutoModelForCausalLM.from_pretrained('./model', model_type='llama')
 
 load_time = time.time()
 
-print(f"\n\nTime taken to load model: {load_time-start} seconds\n\n")
+logger.info(f"Time taken to load model: {load_time-start} seconds")
 
-app = FastAPI()
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-origins = ["*"]
+# Create a lock object so only one request can use the model at a time
+llm_lock = threading.Lock()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# # Create a lock object so only one request can use the model at a time
-# llm_lock = threading.Lock()
-
-@app.post("/")
-async def root(request: Request):
-    prompt = await request.body()
+@app.route("/", methods=["POST"])
+def root():
+    prompt = request.data
     decoded_prompt = prompt.decode('utf-8')
 
     complete_prompt = f"[INST]<<SYS>>You are Socrates.<</SYS>>{decoded_prompt}[/INST]"
 
     def generate():
-        # with llm_lock:  # Acquire the lock before calling the llm function
-        for word in llm(complete_prompt, stream=True):
-            # encoded_word = urllib.parse.quote(word)
-            yield word
+        with llm_lock:  # Acquire the lock before calling the llm function
+            for word in llm(complete_prompt, stream=True):
+                yield word
 
-    return StreamingResponse(generate(), media_type="text/plain")
+    return Response(generate(), content_type="text/plain")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=80)
