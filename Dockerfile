b340fa78973589f890fbf9e63445ee0f1b37bb8b
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-11-22 15:56:44 +0200

    Created a server-sent-events setup

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,7 @@ RUN pip install llama-cpp-python
 RUN pip install ctransformers
 RUN pip install Flask
 
-# RUN wget -O llama-2-7b-chat.ggmlv3.q4_K_S.bin https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q4_K_S.bin?download=true
-
-COPY ./h2o-danube3-500m-chat-Q4_K_M.gguf .
+RUN wget -O llama-2-7b-chat.ggmlv3.q4_K_S.bin https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q4_K_S.bin?download=true
 
 COPY ./main.py .
 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from ctransformers import AutoModelForCausalLM
 import time
 from flask import Flask, request, Response
+import urllib.parse
+import threading
 
 start = time.time()
 
 # Load the pretrained model
-# llm = AutoModelForCausalLM.from_pretrained('TheBloke/Llama-2-7B-Chat-GGML', model_file = 'llama-2-7b-chat.ggmlv3.q2_K.bin' )
-# llm = AutoModelForCausalLM.from_pretrained('h2oai/h2o-danube3-4b-chat-GGUF', model_file = 'h2o-danube3-4b-chat-Q2_K.gguf' )
-llm = AutoModelForCausalLM.from_pretrained('h2oai/h2o-danube3-500m-chat-GGUF', model_file = 'h2o-danube3-500m-chat-Q4_K_M.gguf' )
+llm = AutoModelForCausalLM.from_pretrained('./llama-2-7b-chat.ggmlv3.q4_K_S.bin', model_type='llama')
 
 load_time = time.time()
 
@@ -15,14 +15,19 @@ print(f"\n\nTime taken to load model: {load_time-start} seconds\n\n")
 
 app = Flask('app')
 
+# Create a lock object
+llm_lock = threading.Lock()
+
 @app.route('/', methods=['POST'])
 def root():
     prompt = request.data.decode('utf-8')
 
     def generate():
-        for word in llm(prompt, stream=True):
-            yield word
+        with llm_lock:  # Acquire the lock before calling the llm function
+            for word in llm(prompt, stream=True):
+                encoded_word = urllib.parse.quote(word)
+                yield f"data: {encoded_word}\n\n"
 
-    return Response(generate(), mimetype='text/plain')
+    return Response(generate(), mimetype="text/event-stream")
 
 app.run(host='0.0.0.0')
