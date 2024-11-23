d1ffed9fe6b6b02e0b86c8051999fa6edabf1308
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-11-23 03:04:06 +0200

    Added some error handling due to timeouts on slower hardware

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def prompt():
     return Response(generate(), content_type="text/plain")
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8080, cleanup_interval=600)
